e749c0e4b866f9a29d9aa80b7c59d1a81b9f3173
rexjoseph <josephrex96@gmail.com>
2020-05-10 07:36:00 +0100

    setup keys for database and URIs

--- Users_Server/index.js
+++ Users_Server/index.js
@@ -1,2 +1,11 @@
 // Initialize express package
 // This file initializes the backend server.
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send({ obj: 'object' });
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT);
