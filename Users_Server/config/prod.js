2d16e3bc33ab27cc07b081c3ff6d3788ada41b83
rexjoseph <josephrex96@gmail.com>
2020-05-16 18:19:35 +0100

    delete duplicate files

--- Users_Server/config/prod.js
+++
@@ -1,8 +0,0 @@
-// prod.js file - production keys [feel free to commit this code to GitHub]
-
-module.exports = {
-  GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
-  GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
-  mongoURI: process.env.MONGO_URI,
-  cookieKey: process.env.COOKIE_KEY
-}
