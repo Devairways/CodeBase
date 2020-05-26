a6c16c9eb3551cd888e52efd7f980713ac07833e
Devairways <you@example.com>
2020-05-26 23:00:04 +0200

    strapi setup content types

---
+++ Content_Server/content_api/plugins/blogs/README.md
@@ -0,0 +1,3 @@
+# Strapi plugin blogs
+
+A quick description of blogs.
---
+++ Content_Server/content_api/plugins/blogs/controllers/blogs.js
@@ -0,0 +1,25 @@
+'use strict';
+
+/**
+ * blogs.js controller
+ *
+ * @description: A set of functions called "actions" of the `blogs` plugin.
+ */
+
+module.exports = {
+
+  /**
+   * Default action.
+   *
+   * @return {Object}
+   */
+
+  index: async (ctx) => {
+    // Add your own logic here.
+
+    // Send 200 `ok`
+    ctx.send({
+      message: 'ok'
+    });
+  }
+};
